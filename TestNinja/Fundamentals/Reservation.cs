3b5fbf7453b1e60bf571ff2b8233f4b6a94bedfe
Patrick Meyer <tbt_paddik@hotmail.com>
2018-03-08 16:13:37 +0100

    Add project files.

---
+++ TestNinja/Fundamentals/Reservation.cs
@@ -0,0 +1,19 @@
+﻿namespace TestNinja.Fundamentals
+{
+    public class Reservation
+    {
+        public User MadeBy { get; set; }
+
+        public bool CanBeCancelledBy(User user)
+        {
+            // There are 3 execution paths (scenarios) Admin, same user made reservation and someone else trying.
+            return (user.IsAdmin || MadeBy == user); 
+        }
+        
+    }
+
+    public class User
+    {
+        public bool IsAdmin { get; set; }
+    }
+}
---
+++ TestNinja/UnitTests/TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    // Naming: Method_Scenario_ReturnValue
+    // IS USING MSTESTS
+    [TestFixture]
+    public class ReservationTests
+    {
+        [Test]
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            // Arrange
+            var user = new User { IsAdmin = true };
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_SameUserCancelling_ReturnsTrue()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation { MadeBy = user };
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        [Test]
+        public void CanBeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            // Arrange
+            var user = new User();
+            var reservation = new Reservation();
+
+            // Act
+            var result = reservation.CanBeCancelledBy(user);
+
+            // Assert
+            Assert.IsFalse(result);
+        }
+    }
+}
